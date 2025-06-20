7fa1c01b82ebe3491f74403be5f630943dff497e
Raj Patil <patilraj0726@gmail.com>
2025-06-20 23:12:42 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/internshipdemo/',  // <--- Add this line
+  base: '/internshipdemo/',  
   plugins: [react(), tailwindcss()],
 })
