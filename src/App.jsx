e1738ab28b0db2f7a2f3e15d2cbd14eb95ef481e
Raj Patil <patilraj0726@gmail.com>
2025-06-20 23:21:52 +0530

    Switch to HashRouter for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './pages/Home';
 import AddItem from './pages/AddItem';
 import ViewItems from './pages/ViewItems';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/add" element={<AddItem />} />
-        <Route path="/view" element={<ViewItems />} />
-      </Routes>
-    </Router>
+  <Router>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/add" element={<AddItem />} />
+    <Route path="/view" element={<ViewItems />} />
+  </Routes>
+</Router>
+
   );
 };
 
